1ad5f4e3c0d226f9bf45f078cbcbddb29f8f4368
Nimblen <nnurhodjaev@gmail.com>
2024-01-25 12:57:56 +0300

    fix filter and parser 0.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import catalog, car_filter, product, order_car
+from .views import catalog, product, order_car
 
 
 
@@ -7,7 +7,6 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('', catalog,  name='catalog'),
-    path('filter/', car_filter, name='filter'),
     path('<slug:brand_slug>/<slug:car_slug>/<int:car_id>/', product, name='car_detail'),
     path('order/<int:car_id>/', order_car, name='order_car'),
 
--- catalog/views.py
+++ catalog/views.py
@@ -2,11 +2,13 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.db.models import Count
+
+from catalog.utils.filter_utils import process_filters
+
 # from django.conf import settings
 from .models import Car, CarBrand, Order, Photo, Visit, Video
 from .utils.paginator_utils import paginate_objects
 from blog.models import Post
-
 
 
 # r = redis.Redis(host=settings.REDIS_HOST,
@@ -16,21 +18,34 @@ from blog.models import Post
 
 def index(request):
     cars = Car.objects.all()
-    top_cars = Car.objects.annotate(visits_count=Count('visit')).order_by('-visits_count')[:5]
-    top_posts = Post.objects.annotate(visits_count=Count('visit')).order_by('-visits_count')[:3]
+    top_cars = Car.objects.annotate(visits_count=Count("visit")).order_by(
+        "-visits_count"
+    )[:5]
+    top_posts = Post.objects.annotate(visits_count=Count("visit")).order_by(
+        "-visits_count"
+    )[:3]
     videos = Video.objects.all()
-    context = {"cars": cars, 'top_cars': top_cars, 'top_posts': top_posts, 'videos': videos}
+    context = {
+        "cars": cars,
+        "top_cars": top_cars,
+        "top_posts": top_posts,
+        "videos": videos,
+    }
     return render(request, "catalog/index.html", context)
 
 
-
-
 def catalog(request):
-    sort_by = request.GET.get('sort_by', 'pk')  # значение по умолчанию - 'id'
-    page = request.GET.get('page', 1)
-    per_page = request.GET.get('per_page', 10)
-    sort_view = request.GET.get('g', 'y')
-    if sort_by:
+    sort_by = request.GET.get("sort_by", "pk")  # значение по умолчанию - 'id'
+    page = request.GET.get("page", 1)
+    per_page = request.GET.get("per_page", 10)
+    sort_view = request.GET.get("g", "y")
+    category = request.GET.get("category", "None")
+    filter_car = {}
+    if category == "on":
+        filter_car = process_filters(request)
+    if sort_by and category == "on":
+        order_cars = Car.objects.filter(**filter_car).order_by(sort_by)
+    else:
         order_cars = Car.objects.all().order_by(sort_by)
     count_of_car = Car.objects.count()
     paginated_cars = paginate_objects(order_cars, page, per_page)
@@ -44,7 +59,6 @@ def catalog(request):
     return render(request, "catalog/catalog.html", context)
 
 
-
 def product(request, brand_slug, car_slug, car_id):
     brand_id = CarBrand.objects.get(name=brand_slug)
     car = get_object_or_404(Car, brand=brand_id, slug=car_slug, pk=car_id)
@@ -52,55 +66,13 @@ def product(request, brand_slug, car_slug, car_id):
     ip_address = request.META.get("REMOTE_ADDR")
     Visit.objects.create(user=request.user, ip=ip_address, car_id=car_id)
     v = Visit.objects.filter(car_id=car_id).count()
-    #total_views = r.incr(f'Car:{Car.id}:views')
-    similar_cars = Car.objects.filter(brand = brand_id)[:4]
+    # total_views = r.incr(f'Car:{Car.id}:views')
+    similar_cars = Car.objects.filter(brand=brand_id)[:4]
     context = {"car": car, "visit": v, "photo": photo, "similar_cars": similar_cars}
     return render(request, "catalog/product_page.html", context)
-
 
 
 def order_car(request, car_id):
     car = Car.objects.get(id=car_id)
     Order.objects.create(car=car, user=request.user)
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
-
-
-
-
-def car_filter(request):
-    # Получаем значения фильтров из запроса
-    sort_view = request.GET.get('g', 'y')
-    brand = int(request.GET.get("car_brand_id"))
-    year_from = int(request.GET.get("car_year_from"))
-    year_to = int(request.GET.get("car_year_to"))
-    price_from = request.GET.get("min_price_point")
-    price_to = request.GET.get("max_price_point")
-    # Выполняем фильтрацию данных
-    queryset = Car.objects.all()
-    if brand and brand > 0:
-        queryset = queryset.filter(brand=brand)
-    if year_from and year_from > 0:
-        queryset = queryset.filter(fabrication__gte=year_from)
-
-    if year_to and year_to > 0:
-        queryset = queryset.filter(fabrication__lte=year_to)
-
-    if price_from and price_to:
-        queryset = queryset.filter(price__gte=price_from, price__lte=price_to)
-
-    # #Отображаем отфильтрованные данные в шаблоне
-    paginated_cars = paginate_objects(queryset, page_number=1, per_page=10)
-    context = {
-        "cars": paginated_cars,
-        "price_from": price_from,
-        "price_to": price_to,
-        "page_number": 1,
-        "car_count": queryset.count(),
-        "sort_view": sort_view,
-    }
-    return render(
-        request,
-        "catalog/catalog.html",
-        context,
-    )
-
